e0903f763e1ce811e724cab2a1326a7c5521751e
harruhi <51483322+harruhi@users.noreply.github.com>
2019-07-06 16:15:36 +0900

    error fixed

---
+++ 단어 반복문.[y.py
@@ -0,0 +1,7 @@
+speaking_word = input("반목하고 싶은 말은?")
+x = int(input("얼마나 반복하실건가요?"))
+for speaking_word_print in speaking_word:
+   if speaking_word == ():
+      print("단어를 입력하세요!")
+   else:
+      print(speaking_word_print * x)
