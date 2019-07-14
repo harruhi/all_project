e89324e1f53c169b29e656fb6333aff809dc36fc
harruhi <51483322+harruhi@users.noreply.github.com>
2019-07-14 22:35:08 +0900

    horray!

--- def연산자_활용_수학문제풀기.py
+++ def연산자_활용_수학문제풀기.py
@@ -1,36 +1,25 @@
 x = input("곱하기, 나누기, 더하기, 빼기 중에서 고르세여")
 y = int(input("수를 적어주세요."))
 z = int(input("변환 시킬값을 써주세요."))
-Number_Return_Division = ()
-Number_Return_Plus = ()
-Number_Return_Minus = ()
-Number_Return_Multi = ()
+
 #함수정의 
-def Plus_plux(y, z):
-    y + z == Number_Return_Plus
-    return Number_Return_Plus
-    print(Number_Return_Plus)
-def Minus_Tanos(y, z):
-    y - z == Number_Return_Minus
-    return Number_Return_Minus
-    print(Number_Return_Minus)
-def Multiple_Multi(y, z):
-    y * z == Number_Return_Multi
-    return Number_Return_Multi
-    print(Number_Return_Multi)
-def Division_Diamond(y, z):
-    y / z == Number_Return_Division
-    return Number_Return_Division
-    print(Number_Return_Division)
+def Plus_plux(number1, number2):
+    return number1 + number2
+def Minus_Tanos(number3, number4):
+    return number3 - number4
+def Multiple_Multi(number5, number6):
+    return number5 * number6
+def Division_Diamond(number7, number8):
+    return number7 / number8
 
 #진짜 코딩
-if x == '곱하기':
-    Multiple_Multi
-if x == '나누기':
-    Division_Diamond
-if x == '더하기':
-    Plus_plux
-if x == '빼기':
-    Minus_Tanos
+if x == "곱하기":
+    Multiple_Multi(y, z)
+if x == "나누기":
+    Division_Diamond(y, z)
+if x == "더하기":
+    Plus_plux(y, z)
+if x == "빼기":
+    Minus_Tanos(y, z)
 else:
     print("다시 써주세요!")
